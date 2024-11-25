c17bf7f52c7634ed13f0c0226d16d30e1e4483f8
Emre Camasuvi <emre@camasuvi.net>
2024-11-25 17:58:45 +0300

    Introduce the initial field to guess input

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import { sample } from '../../utils';
-import { WORDS } from '../../data';
+import { sample } from "../../utils";
+import { WORDS } from "../../data";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +9,32 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [word, setWord] = React.useState("");
+  const LIMIT = 5;
+  const submission = (e) => {
+    e.preventDefault();
+    if (word.length === LIMIT) {
+      setWord("");
+      console.log(word);
+    }
+  };
+  return (
+    <>
+      <form onSubmit={submission} class="guess-input-wrapper">
+        <label for="guess-input">Enter guess: {word}</label>
+        <input
+          autoComplete="password"
+          required
+          id="guess-input"
+          minLength={LIMIT}
+          maxLength={LIMIT}
+          type="text"
+          value={word}
+          onChange={(e) => setWord(e.target.value.toUpperCase())}
+        />
+      </form>
+    </>
+  );
 }
 
 export default Game;
